aa9fa91b75ab6002cb0b0eb50e078be327dd1c3f
Tugdual Grall <tgrall@github.com>
2022-08-03 14:43:27 +0000

    add workflow info

--- src/run.ts
+++ src/run.ts
@@ -1,10 +1,18 @@
 import * as core from '@actions/core'
 import {HttpClient} from '@actions/http-client'
+import * as gh from './github'
 
 export async function run(): Promise<void> {
     try {
         core.info('Inside run block');
+        const ghToken: string = core.getInput('repo-token', {required: true});
 
+        // authenticated client to call APIs
+        const client: HttpClient = gh.getClient(ghToken);
+
+        // get workflow id see https://docs.github.com/en/actions/learn-github-actions/environment-variables
+        const workflowId: string = process.env['GITHUB_RUN_ID'] || '';
+        core.info("workflowId: " + workflowId);
 
     } catch (e) {
         core.setFailed(`Run failed: ${e}`);
