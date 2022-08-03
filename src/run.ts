cc4dd4156be228521d6e9c3c53095d22933c69a8
Tugdual Grall <tgrall@github.com>
2022-08-03 17:39:21 +0000

    add reusable workflow

--- src/run.ts
+++ src/run.ts
@@ -2,6 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {HttpClient} from '@actions/http-client'
 import * as gh from './github'
+import {createWriteStream} from 'fs'
 
 export async function run(): Promise<void> {
     try {
@@ -20,26 +21,30 @@ export async function run(): Promise<void> {
         const context = JSON.stringify(github.context, undefined, 2)
         core.info(`The event context: ${context}`);
 
-        const workflowLogFile: string = await gh.fetchLogsForWorkflow(client, repo, workflowId);
-        core.debug(`workflow-log-file : ${workflowLogFile}`);
-        core.setOutput('workflow-log-file', workflowLogFile );
+        // const workflowLogFile: string = await gh.fetchLogsForWorkflow(client, repo, workflowId);
+        // core.debug(`workflow-log-file : ${workflowLogFile}`);
+        // core.setOutput('workflow-log-file', workflowLogFile );
 
 
-        // const jobs: gh.Job[] = await gh.fetchJobs(
-        //     client,
-        //     repo,
-        //     workflowId,
-        //     []
-        //   )
+        const jobs: gh.Job[] = await gh.fetchJobs(
+            client,
+            repo,
+            workflowId,
+            []
+          )
           
-        // core.debug(`Getting logs for ${jobs.length} jobs for workflow ${workflowId}`);
-        // for (const j of jobs) {
-        //     const lines: string[] = await gh.fetchLogsForJob(client, repo, j);
-        //     core.debug(`Fetched ${lines.length} lines for job ${j.name}`);
+        core.debug(`Getting logs for ${jobs.length} jobs for workflow ${workflowId}`);
+        for (const j of jobs) {
+            const lines: string[] = await gh.fetchLogsForJob(client, repo, j);
+            core.debug(`Fetched ${lines.length} lines for job ${j.name}`);
       
-        //     const tmpfile = `./out-${j.id}.log`;
-        //     core.debug(`Writing to ${tmpfile}`);
-        // }
+            const tmpfile = `./out-${j.id}.log`;
+            core.info(`Writing to ${tmpfile}`);
+            const out = createWriteStream(tmpfile);
+            out.write(lines);
+            out.end();            
+
+        }
 
 
     } catch (e) {
