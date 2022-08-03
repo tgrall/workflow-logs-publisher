e2fe82bea204bbad3f9e1d4fe38023af6b5a5932
Tugdual Grall <tgrall@github.com>
2022-08-03 17:55:20 +0000

    add reusable workflow

--- src/run.ts
+++ src/run.ts
@@ -21,30 +21,30 @@ export async function run(): Promise<void> {
         const context = JSON.stringify(github.context, undefined, 2)
         core.info(`The event context: ${context}`);
 
-        // const workflowLogFile: string = await gh.fetchLogsForWorkflow(client, repo, workflowId);
-        // core.debug(`workflow-log-file : ${workflowLogFile}`);
-        // core.setOutput('workflow-log-file', workflowLogFile );
+        const workflowLogFile: string = await gh.fetchLogsForWorkflow(client, repo, workflowId);
+        core.debug(`workflow-log-file : ${workflowLogFile}`);
+        core.setOutput('workflow-log-file', workflowLogFile );
 
 
-        const jobs: gh.Job[] = await gh.fetchJobs(
-            client,
-            repo,
-            workflowId,
-            []
-          )
+        // const jobs: gh.Job[] = await gh.fetchJobs(
+        //     client,
+        //     repo,
+        //     workflowId,
+        //     []
+        //   )
           
-        core.debug(`Getting logs for ${jobs.length} jobs for workflow ${workflowId}`);
-        for (const j of jobs) {
-            const lines: string[] = await gh.fetchLogsForJob(client, repo, j);
-            core.debug(`Fetched ${lines.length} lines for job ${j.name}`);
+        // core.debug(`Getting logs for ${jobs.length} jobs for workflow ${workflowId}`);
+        // for (const j of jobs) {
+        //     const lines: string[] = await gh.fetchLogsForJob(client, repo, j);
+        //     core.debug(`Fetched ${lines.length} lines for job ${j.name}`);
       
-            const tmpfile = `./out-${j.id}.log`;
-            core.info(`Writing to ${tmpfile}`);
-            const out = createWriteStream(tmpfile);
-            out.write(lines);
-            out.end();            
+        //     const tmpfile = `./out-${j.id}.log`;
+        //     core.info(`Writing to ${tmpfile}`);
+        //     const out = createWriteStream(tmpfile);
+        //     out.write(lines);
+        //     out.end();            
 
-        }
+        // }
 
 
     } catch (e) {
