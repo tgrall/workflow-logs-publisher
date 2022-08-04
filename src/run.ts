240f03764083179e8a63d55e0cb2457e4f77f9d1
Tugdual Grall <tgrall@github.com>
2022-08-04 07:26:06 +0000

    save-artifact-from-code-3

--- src/run.ts
+++ src/run.ts
@@ -5,37 +5,45 @@ import * as gh from './github'
 import * as artifact from '@actions/artifact'
 const artifactClient = artifact.create()
 
+// Split comma separated inputs into an array of trimmed values
+export function getCommaSeparatedInput(value: string): string[] {
+    let retVal: string[] = []
+    if (value !== '') {
+      retVal = value.split(',')
+      // trim array items
+      retVal = retVal.map(s => s.trim())
+    }
+  
+    return retVal
+  }
 
 export async function run(): Promise<void> {
     try {
         const ghToken: string = core.getInput('repo-token', {required: true});
-
+        const jobNames: string = core.getInput('job-names', {required: false}) || '';
+        const allowList = getCommaSeparatedInput(jobNames);
         // authenticated client to call APIs
         const client: HttpClient = gh.getClient(ghToken);
 
         // get workflow id see https://docs.github.com/en/actions/learn-github-actions/environment-variables
-        const workflowId: string = github.context.runId.toString() || '';
+
+        // get all the jobs for the current workflow
+        const workflowId: string = process.env['GITHUB_RUN_ID'] || ''
         const repo: string = process.env['GITHUB_REPOSITORY'] || ''
-        core.debug(`workflowId: ${workflowId}`);
-        core.debug(`repo: ${repo}`);
-        
-
-        const context = JSON.stringify(github.context, undefined, 2)
-        core.info(`The event context: ${context}`);
-
-        // const workflowLogFile: string = await gh.fetchLogsForWorkflow(client, repo, workflowId);
-        // core.debug(`workflow-log-file : ${workflowLogFile}`);
-        // core.setOutput('workflow-log-file', workflowLogFile );
-
-
+        core.debug(`Allow listing ${allowList.length} jobs in repo ${repo}`)
         const jobs: gh.Job[] = await gh.fetchJobs(
             client,
             repo,
             workflowId,
-            []
-          )
+            allowList
+        )        // const workflowLogFile: string = await gh.fetchLogsForWorkflow(client, repo, workflowId);
+        // core.debug(`workflow-log-file : ${workflowLogFile}`);
+        // core.setOutput('workflow-log-file', workflowLogFile );
+
+
         let workflowLogFiles: string[] = [];
         core.info(`Getting logs for ${jobs.length} jobs for workflow ${workflowId}`);
+        core.info( JSON.stringify(jobs, null, 2) );
         for (const j of jobs) {
             const tmpfile: string = await gh.fetchLogsForJob(client, repo, j);      
             core.info(`Writing to ${tmpfile}`);
